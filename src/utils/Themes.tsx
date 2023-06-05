7d6e6d7eec775d52723e742af496d78eb29bb9c3
Balu Babu <balub997@gmail.com>
2023-06-06 02:35:07 +0530

    chore: bumped up nextjs version to v13

--- src/utils/Themes.tsx
+++
@@ -1,9 +0,0 @@
-import { createTheme } from "@mui/material/styles";
-
-const theme = createTheme({
-  typography: {
-    fontFamily: ["Cabin", "Open Sans"].join(","),
-  },
-});
-
-export default theme;
